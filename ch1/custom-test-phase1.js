6077c477f387c813e1dd89432106491cee793cfc
royo <roy@osherove.com>
2019-07-30 13:49:35 +0300

    added "ch1" to npm run

--- ch1/custom-test-phase1.js
+++ ch1/custom-test-phase1.js
@@ -11,7 +11,7 @@ const parserTest = () => {
     let np = new NumberParser();
     let result = np.sum("1,2");
     if (result === 3) {
-      console.log("parserTest PASSED");
+      console.log("parserTest example 1 PASSED");
     } else {
       throw new Error("parserTest: expected 3 but was ${result}");
     }
