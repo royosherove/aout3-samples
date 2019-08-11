9ddb23431cde57baa3291347f1982db58efa638a
royo <roy@osherove.com>
2019-08-12 02:32:16 +0300

    ch1: changes example with state to totalSoFar custom test 2

--- ch1/number-parser3.js
+++ ch1/number-parser3.js
@@ -1,6 +1,15 @@
 'use strict';
 
 const winston = require('winston');
+let total = 0;
+
+/**
+ *
+ * @returns {number}
+ */
+const totalSoFar = () => {
+  return total;
+};
 
 const makeLogger = () => {
   return winston
@@ -11,28 +20,20 @@ const makeLogger = () => {
 };
 
 const logger = makeLogger();
-let wasCalled = false;
-
-/**
-   *
-   * @returns {boolean}
-   */
-const wasSumCalled = () => {
-  return wasCalled;
-};
 
 const sum = (numbers) => {
-  wasCalled = true;
   const [a, b] = numbers.split(',');
   logger.info(
     'this is a very important log output',
     { firstNumWas: a, secondNumWas: b });
 
-  return Number.parseInt(a, 10) + Number.parseInt(b, 10);
+  const result = Number.parseInt(a, 10) + Number.parseInt(b, 10);
+  total += result;
+  return result;
 };
 
 module.exports = {
-  wasSumCalled,
+  totalSoFar,
   sum
 };
 // run this file with node (name of this file) to see the logging
