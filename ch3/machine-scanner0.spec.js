4395c47a3b6235b7f94f21981a8de59de4e0950e
royo <royo>
2019-09-28 23:58:09 +0300

    aaa spacing

--- ch3/machine-scanner0.spec.js
+++ ch3/machine-scanner0.spec.js
@@ -5,19 +5,17 @@ const { findRecentlyRebooted } = require('./machine-scanner0');
 describe('findRecentlyRebooted', () => {
   test('given no machines, returns empty results', () => {
     const result = findRecentlyRebooted([], 0);
+
     expect(result.length).toBe(0);
   });
 
-  test('moments', () => {
-    const m1 = moment(Date.now()).subtract(2, 'days');
-    const diff = moment().diff(m1, 'days');
-    console.log(diff);
-  });
 
   test('given one machine over than threhold, it is ignored', () => {
     const twoDaysAgo = moment().subtract(2, 'days');
     const machines = [{ lastBootTime: twoDaysAgo, name: 'machine1' }];
+
     const result = findRecentlyRebooted(machines, 1);
+
     expect(result.length).toBe(0);
   });
 
@@ -27,14 +25,18 @@ describe('findRecentlyRebooted', () => {
       { lastBootTime: twoDaysAgo, name: 'should-be-ignored' },
       { lastBootTime: Date.now(), name: 'should-be-found' }
     ];
+
     const result = findRecentlyRebooted(machines, 1);
+
     expect(result.length).toBe(1);
     expect(result[0].name).toContain('should-be-found');
   });
 
   test('given one machine less than threhold, returns its name and boot time', () => {
     const machines = [{ lastBootTime: Date.now(), name: 'machine1' }];
+
     const result = findRecentlyRebooted(machines, 1);
+
     expect(result.length).toBe(1);
   });
 });
