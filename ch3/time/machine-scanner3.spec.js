d20c69d3c5fa07abaaf60de0439585286d3572fc
royo <royo>
2019-10-17 01:51:31 +0300

    fixes machine scanner class

--- ch3/time/machine-scanner3.spec.js
+++ ch3/time/machine-scanner3.spec.js
@@ -1,12 +1,33 @@
 const { machineScanner } = require('./machine-scanner3');
 describe('machineScanner', () => {
   describe('findRecentlyRebooted', () => {
-    test('given one of two machines under the threshold, it is found', () => {
+    test('given 1 of 2 machines under the threshold, it is found', () => {
       const fromDate = new Date('01 03 2000');
       const scanner = new machineScanner(fromDate);
       const rebootTwoDaysEarly = new Date('01 01 2000');
-      const machines = [{ lastBootTime: rebootTwoDaysEarly, name: 'ignored' },
+      const machines = [
+        { lastBootTime: rebootTwoDaysEarly, name: 'ignored' },
         { lastBootTime: fromDate, name: 'found' }];
+
+      const result = scanner.findRecentlyRebooted(machines, 1);
+
+      expect(result.length).toBe(1);
+      expect(result[0].name).toContain('found');
+    });
+  });
+});
+
+const makeScanner = date => new machineScanner(date);
+
+describe('v2 machineScanner', () => {
+  describe('findRecentlyRebooted', () => {
+    test('given 1 of 2 machines under the threshold, it is found', () => {
+      const fromDate = new Date('01 03 2000');
+      const scanner = makeScanner(fromDate);
+      const rebootTwoDaysEarly = new Date('01 01 2000');
+      const machines = [
+          { lastBootTime: rebootTwoDaysEarly, name: 'ignored' },
+          { lastBootTime: fromDate, name: 'found' }];
 
       const result = scanner.findRecentlyRebooted(machines, 1);
 
