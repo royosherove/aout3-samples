c931b37dc6f92720ab11225af73259665752df6e
royo <royo>
2019-10-18 01:28:44 +0300

    requireCache not working still. because jest ignores require.cache. requires not using jest?

--- ch3/time/04-module-patching/machine-scanner4.requireCache.spec.js
+++ ch3/time/04-module-patching/machine-scanner4.requireCache.spec.js
@@ -1,23 +1,22 @@
 const dataModulePath = require.resolve('./my-data-module');
- ;
 
 const overrideDataModule = fakeData => {
   delete require.cache[dataModulePath];
-  const scannerModule = require('./machine-scanner4');
-  scannerModule.require.cache[dataModulePath] = {
+  // const scannerModule = require('./machine-scanner4');
+  require.cache[dataModulePath] = {
     id: dataModulePath,
     filename: dataModulePath,
-    loaded: false,
+    loaded: true,
     exports: {
       getAllMachines: () => fakeData,
     }
-  }
+  };
+  require(dataModulePath);
 };
 
 
 
 describe('findRecentlyRebooted', () => {
-  beforeEach(() => delete require.cache[dataModulePath]);
 
   test('given 1 of 2 machines under the threshold, it is found', () => {
     const rebootTwoDaysEarly = new Date("01 01 2000");
@@ -26,9 +25,7 @@ describe('findRecentlyRebooted', () => {
       {lastBootTime: rebootTwoDaysEarly, name: 'ignored'},
       {lastBootTime: fromDate, name: 'found'}
     ]);
-    console.log(require.cache);
     const { findRecentlyRebooted } = require('./machine-scanner4');
-    console.log(require.cache);
     const result = findRecentlyRebooted(1, new Date("01 03 2000"));
 
     expect(result.length).toBe(1);
