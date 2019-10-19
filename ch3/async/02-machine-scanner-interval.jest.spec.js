8828c0a731b693124eb9d5fbc0c2ac22431ab261
royo <royo>
2019-10-20 00:21:15 +0300

    02-machine-scanner-interval.jest.spec.js

--- ch3/async/02-machine-scanner-interval.jest.spec.js
+++ ch3/async/02-machine-scanner-interval.jest.spec.js
@@ -1,79 +1,20 @@
+const {  findRebootedPerInterval } = require('./machine-scanner-interval');
 
-const { findRecentlyRebooted, findRebootedPerInterval } = require('./machine-scanner-interval');
 
-// we have to get over setinterval
 describe('findRebootedPerInterval', () => {
-  test('faketimer default with setInterval - is impossible to test', done => {
-    // faking global setinterval
+  beforeEach(jest.clearAllTimers);
+
+  test('findRebootedPerInterval, when found, triggers onFound callback', () => {
     jest.useFakeTimers();
-    const fromDate = new Date('01 03 2000');
-    const rebootTwoDaysEarly = new Date('01 01 2000');
-    const machines = [{ lastBootTime: rebootTwoDaysEarly, name: 'ignored' },
-      { lastBootTime: fromDate, name: 'found' }];
+    const machines = [
+      { lastBootTime: new Date('01 01 2000'), name: 'ignored' },
+      { lastBootTime: new Date('01 03 2000'), name: 'found' }];
     let found = false;
-    findRebootedPerInterval(() => machines,
-        () => {
-          found = true;
-          console.log('got callback!!');
-          done();
-        }, 1, 2, () => fromDate);
+    findRebootedPerInterval(() => machines,//getMachines
+        () =>  found = true ,             //onFound callback
+        2,                                // maxDays
+        () => new Date("01 03 2000"));    //getDate
     jest.advanceTimersToNextTimer();
     expect(found).toEqual(true);
-  });
-  test('default with setInterval - is impossible to test', done => {
-    // faking global setinterval
-    setInterval = (cb, time) => cb();
-    const fromDate = new Date('01 03 2000');
-    const rebootTwoDaysEarly = new Date('01 01 2000');
-    const machines = [{ lastBootTime: rebootTwoDaysEarly, name: 'ignored' },
-      { lastBootTime: fromDate, name: 'found' }];
-    let found = false;
-    findRebootedPerInterval(() => machines,
-      () => {
-        found = true;
-        console.log('got callback!!');
-        done();
-      }, 1, 2, () => fromDate);
-    expect(found).toEqual(true);
-  });
-});
-describe('findRecentlyRebooted', () => {
-  test('given no machines, returns empty results', () => {
-    const someDate = new Date('01 01 2000');
-
-    const result = findRecentlyRebooted([], 0, someDate);
-
-    expect(result.length).toBe(0);
-  });
-
-  test('given one machine over the threshold, it is ignored', () => {
-    const fromDate = new Date('01 03 2000');
-    const rebootTwoDaysEarly = new Date('01 01 2000');
-    const machines = [{ lastBootTime: rebootTwoDaysEarly, name: 'machine1' }];
-
-    const result = findRecentlyRebooted(machines, 1, () => fromDate);
-
-    expect(result.length).toBe(0);
-  });
-
-  test('given one of two machines under the threshold, it is found', () => {
-    const fromDate = new Date('01 03 2000');
-    const rebootTwoDaysEarly = new Date('01 01 2000');
-    const machines = [{ lastBootTime: rebootTwoDaysEarly, name: 'ignored' },
-      { lastBootTime: fromDate, name: 'found' }];
-
-    const result = findRecentlyRebooted(machines, 1, () => fromDate);
-
-    expect(result.length).toBe(1);
-    expect(result[0].name).toContain('found');
-  });
-
-  test('given one machine less than threshold, returns its name and boot time', () => {
-    const fromDate = new Date('01 01 2000');
-    const machines = [{ lastBootTime: fromDate, name: 'any-name' }];
-
-    const result = findRecentlyRebooted(machines, 1, () => fromDate);
-
-    expect(result.length).toBe(1);
   });
 });
