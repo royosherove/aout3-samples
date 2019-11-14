4c731c3ea71ace458f5ac3ee46ca6662be6c789f
royo <royo>
2019-11-14 09:51:49 +0200

    async samples

---
+++ ch4/async-samples.js
@@ -0,0 +1,22 @@
+const calculate1 = (x, y, resultCallback) => {
+    setTimeout(() => resultCallback(x + y),
+        10000)
+};
+
+const calculate2 = (x, y) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => resolve(x + y),
+            10000)
+    });
+};
+
+const calculate3 = async (x, y) => {
+    await setTimeout(null, 10000);
+    return x + y;
+};
+
+module.exports = {
+    calculate1,
+    calculate2,
+    calculate3
+};
