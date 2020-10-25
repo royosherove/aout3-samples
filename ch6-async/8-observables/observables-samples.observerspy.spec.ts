cf5aba7e3965d24895d011baccd6c7cc63f858ae
royo <roy@osherove.com>
2020-10-25 02:14:59 +0200

    ch6 logical assert anti pattern

--- ch6-async/8-observables/observables-samples.observerspy.spec.ts
+++ ch6-async/8-observables/observables-samples.observerspy.spec.ts
@@ -12,5 +12,15 @@ describe('with observerspy test', () => {
         ])
     }));
 });
+describe('anti pattern on the assert', () => {
+    afterEach(autoUnsubscribe);
+    test('this test has fake timers', fakeTime((flush) => {
+        const spy = subscribeSpyTo(pizzaCookBook$());
+        flush();
+        expect(spy.getValues())
+            .toEqual(ingredients.map(i => i.name));
+        //this assert is tighly coupled to implementation logic
+    }));
+});
 
 
